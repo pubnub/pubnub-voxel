63c6237513ac35da903a6901c29b49fb0da89aaa
Ian Jennings <ian@meetjennings.com>
2014-08-19 17:46:14 -0500

    get textures working

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ module.exports = function(opts, setup) {
   var defaults = {
     generate: voxel.generator['Valley'],
     chunkDistance: 2,
-    materials: ['#fff', '#000'],
-    materialFlatColor: true,
+    materials: [
+      ['grass', 'dirt', 'grass_dirt']
+    ],
+    texturePath: '../textures/',
     worldOrigin: [0, 0, 0],
     controls: { discreteFire: true }
   }
   opts = extend({}, defaults, opts || {})
 
-  // setup the game and add some trees
+  // setup the game 
   var game = createGame(opts)
   var container = opts.container || document.body
   window.game = game // for debugging
@@ -40,35 +42,11 @@ module.exports = function(opts, setup) {
 
 function defaultSetup(game, avatar) {
   
-  var makeFly = fly(game)
   var target = game.controls.target()
-  game.flyer = makeFly(target)
   
-  // highlight blocks when you look at them, hold <Ctrl> for block placement
-  var blockPosPlace, blockPosErase
-  var hl = game.highlighter = highlight(game, { color: 0xff0000 })
-  hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos })
-  hl.on('remove', function (voxelPos) { blockPosErase = null })
-  hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
-  hl.on('remove-adjacent', function (voxelPos) { blockPosPlace = null })
-
   // toggle between first and third person modes
   window.addEventListener('keydown', function (ev) {
     if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle()
-  })
-
-  // block interaction stuff, uses highlight data
-  var currentMaterial = 1
-
-  game.on('fire', function (target, state) {
-    var position = blockPosPlace
-    if (position) {
-      game.createBlock(position, currentMaterial)
-    }
-    else {
-      position = blockPosErase
-      if (position) game.setBlock(position, 0)
-    }
   })
 
   game.on('tick', function() {
