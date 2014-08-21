520377e48727ef8b1e8316c8772709f1e932cc37
Ian Jennings <ian@meetjennings.com>
2014-08-21 01:11:25 -0400

    mutliplayer

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ var extend = require('extend')
 var fly = require('voxel-fly')
 var skin = require('minecraft-skin')
 var walk = require('voxel-walk')
-var pubnub = require('pubnub')
+var uuid = window.PUBNUB.uuid();
+var pubnub = window.PUBNUB.init({
+  publish_key: 'demo',
+  subscribe_key: 'demo',
+  uuid: uuid
+});
 
-var channel = 'pubnub-minecraft'
-
-var a_player = function(game) {
-  
-  var createPlayer = player(game);
-  return createPlayer('player.png');
-
-}
+var channel = 'pubnub-voxel';
 
 module.exports = function(opts, setup) {
-  setup = setup || defaultSetup
+  setup = setup || false;
   var defaults = {
     generate: voxel.generator['Valley'],
     chunkDistance: 2,
@@ -41,33 +39,74 @@ module.exports = function(opts, setup) {
 
   // create the player from a minecraft skin file and tell the
   // game to use it as the main player
-  var avatar = a_player(game);
-  avatar.possess()
-  avatar.yaw.position.set(2, 4, 4)
+  var createPlayer = player(game);
 
-  var viking = a_player(game);
-  viking.position.set(2, 4, 4)
+  var Users = {};
+  var User = function(uuid) {
+    
+    var self = {};
 
-  setup(game, avatar)
-  
-  return game
-}
+    self.avatar = createPlayer('player.png');
 
-function defaultSetup(game, avatar) {
-  
-  var target = game.controls.target()
+    self.avatar.position.set(2, 4, 4)
+
+    Users[uuid] = self;
+
+    return self;
+
+  }
+
+  pubnub.subscribe({
+    channel: channel,
+    callback: function(data) {
+      if(data.uuid !== self.uuid) {
+        console.log('got user pos')
+        Users[data.uuid].avatar.position.set(data.position.x, data.position.y, data.position.z);
+      } 
+    },
+    presence: function(data) {
+      if(data.action == "join" && typeof Users[data.uuid] == "undefined") {
+        console.log('user join')
+        new User(data.uuid);
+      }
+      if(data.action == "leave") {
+        delete Users[data.uuid];
+      }
+    }
+  });
+
+  var self = new User(uuid);
+
+  self.avatar.possess()
+  self.avatar.yaw.position.set(2, 4, 4)
+
+  // var viking = new User();
+  // viking.position.set(2, 4, 4)
   
   // toggle between first and third person modes
   window.addEventListener('keydown', function (ev) {
-    if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle()
-  })
+  
+    if (ev.keyCode === 'R'.charCodeAt(0)) self.avatar.toggle()
+
+    pubnub.publish({
+      channel: channel,        
+      message: {
+        uuid: uuid,
+        position: self.avatar.position
+      }
+    });
+
+  });
 
   game.on('tick', function() {
-    walk.render(target.playerSkin)
-    var vx = Math.abs(target.velocity.x)
-    var vz = Math.abs(target.velocity.z)
+
+    walk.render(self.avatar.playerSkin)
+    var vx = Math.abs(self.avatar.velocity.x)
+    var vz = Math.abs(self.avatar.velocity.z)
     if (vx > 0.001 || vz > 0.001) walk.stopWalking()
     else walk.startWalking()
+
   })
 
+  return game
 }
